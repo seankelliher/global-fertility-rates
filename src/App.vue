a1fa4aeab9105719f15a4bdfcafa70a40cf2c2ab
Sean Kelliher <sbkelliher@icloud.com>
2023-10-19 16:22:04 -0400

    Modify order of components.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import HeaderPart from "./components/HeaderPart.vue";
+import AboutMethods from "./components/AboutMethods.vue";
 import PeakNations from "./components/PeakNations.vue";
-import RegionalLists from "./components/RegionalLists.vue";
-import RegionalAvgs from "./components/RegionalAvgs.vue";
 import SearchNations from "./components/SearchNations.vue";
 import AllNations from "./components/AllNations.vue";
-import AboutMethods from "./components/AboutMethods.vue";
+import RegionalLists from "./components/RegionalLists.vue";
+import RegionalAvgs from "./components/RegionalAvgs.vue";
 </script>
 
 <template>
@@ -17,10 +17,10 @@ import AboutMethods from "./components/AboutMethods.vue";
             </div>
             <div class="statistics">
                 <PeakNations  />
-                <RegionalLists />
-                <RegionalAvgs />
                 <SearchNations />
                 <AllNations />
+                <RegionalLists />
+                <RegionalAvgs />
             </div>
         </main>
     </div>
