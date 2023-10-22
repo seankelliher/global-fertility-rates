071c87df2dd203c60a65fe9c6bffd6f22295547e
Sean Kelliher <sbkelliher@icloud.com>
2023-10-22 13:05:11 -0400

    Remove 'display' class. Not needed.

--- src/components/AboutMethods.vue
+++ src/components/AboutMethods.vue
@@ -3,10 +3,8 @@
 
 <template>
     <section class="box-for-intro">
-        <div class="display">
-            <h2>What is a Total Fertility Rate?</h2>
-            <p>Total Fertility Rate or TFR compares figures for the average number of children that would be born per woman if all women lived to the end of their childbearing years and bore children according to a given fertility rate at each age. TFR is a more direct measure of the level of fertility than the crude birth rate, since it refers to births per woman.</p>
-        </div>
+        <h2>What is a Total Fertility Rate?</h2>
+        <p>Total Fertility Rate or TFR compares figures for the average number of children that would be born per woman if all women lived to the end of their childbearing years and bore children according to a given fertility rate at each age. TFR is a more direct measure of the level of fertility than the crude birth rate, since it refers to births per woman.</p>
     </section>
 </template>
 
