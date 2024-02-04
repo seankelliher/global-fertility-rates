62fa30c62a769bf128971e8b89b36d0a49340693
Sean Kelliher <sbkelliher@icloud.com>
2024-02-03 20:58:32 -0500

    Modify map to 'v11'. Add pop-ups, click nation on map, see tfr.

--- src/components/WorldMap.vue
+++ src/components/WorldMap.vue
@@ -12,25 +12,23 @@ const mapContainer = ref(null);
 onMounted(() => {
     map.value = new mapboxgl.Map({
         container: mapContainer.value,
-        style: "mapbox://styles/mapbox/light-v11",
+        style: "mapbox://styles/mapbox/streets-v11", // v11 flat map, v12 globe.
         center: [22, 16],
-        zoom: 1.4
+        zoom: 1.6
     });
 
     map.value.on("load", () => {
-        // Add source for country polygons using the Mapbox Countries tileset
-        // The polygons contain an ISO 3166 alpha-3 code which can be used to for joining the data.
+        // Mapbox Countries tileset has polygons for nations.
+        // Each polygon has an ISO 3166 alpha-3 code used for joining data.
         // https://docs.mapbox.com/vector-tiles/reference/mapbox-countries-v1
         map.value.addSource("countries", {
             type: "vector",
             url: "mapbox://mapbox.country-boundaries-v1"
         });
 
-        // Build a GL match expression that defines the color for every vector tile feature
-        // Use the ISO 3166-1 alpha 3 code as the lookup key for the country shape
+        // Using GL match expression give each polygon a color.
         const matchExpression = ["match", ["get", "iso_3166_1_alpha_3"]];
 
-        // Calculate color values for each country based on "tfr" value
         for (const nation of nations) {
             let color;
 
@@ -39,7 +37,7 @@ onMounted(() => {
             }
 
             if (nation["tfr"] === 0 ) {
-                color = "#fff"; // 95 blue
+                color = "#fff"; // white
             }
 
             if (nation["tfr"] < 1.5 ) {
@@ -85,21 +83,18 @@ onMounted(() => {
             matchExpression.push(nation["code"], color);
         }
 
-        // Last value is the default, used where there is no data
+        // Last value is the default, used where there is no data.
         matchExpression.push("rgba(0, 0, 0, 0)");
 
-
-        // The mapbox.country-boundaries-v1 tileset includes multiple polygons for some
+        // mapbox.country-boundaries-v1 tileset includes multiple polygons for
         // countries with disputed borders. The following expression filters the
-        // map view to show the "US" perspective of borders for disputed countries.
-        // Other world views are available, for more details, see the documentation
-        // on the "worldview" feature property at
+        // map view to show the "US" perspective for disputed borders.
         // https://docs.mapbox.com/data/tilesets/reference/mapbox-countries-v1/#--polygon---worldview-text
         const WORLDVIEW = "US";
         const worldview_filter = [ "all", [ "==", ["get", "disputed"], "false" ], [ "any", [ "==", "all", ["get", "worldview"] ], [ "in", WORLDVIEW, ["get", "worldview"] ] ] ];
 
-        // Add layer from the vector tile source to create the choropleth
-        // Insert it below the "admin-1-boundary-bg" layer in the style
+        // Add layer from the vector tile source to create the choropleth.
+        // Insert it below the "admin-1-boundary-bg" layer in the style.
         map.value.addLayer(
             {
                 "id": "countries-join",
@@ -113,6 +108,34 @@ onMounted(() => {
             },
             "admin-1-boundary-bg"
         );
+
+        map.value.on("click", "countries-join", (e) => {
+            let rate;
+
+            function getRate(x) {
+                nations.forEach(function (nation, index) {
+                    if (nation["code"] === x) {
+                        rate = nations[index].tfr;
+                    }
+                }); 
+            }
+            getRate(e.features[0].properties.iso_3166_1_alpha_3);
+
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(`<p>${e.features[0].properties.name_en}: ${rate} births per woman</p>`)
+                .addTo(map.value);
+        });
+
+        // Change cursor from "hand" to "hand with pointed finger" over nation.
+        map.value.on("mouseenter", "countries-join", () => {
+            map.value.getCanvas().style.cursor = "pointer";
+        });
+         
+        // Change cursor "hand" when not over nation.
+        map.value.on("mouseleave", "countries-join", () => {
+            map.value.getCanvas().style.cursor = "";
+        });
     });
 });
 
@@ -126,7 +149,7 @@ onUnmounted(() => {
 <template>
     <section class="box-for-map">
         <div ref="mapContainer" class="map-container"></div>
-        <div class="legend">
+        <div class="map-legend">
             <svg>
                 <rect width="1.4em" height="1.4em" fill="#fff" stroke="#bdbdbd">
                     <title>no births or map data</title>
