1f129991e8c17a309db8c79e3922d720923d8884
Sean Kelliher <sbkelliher@icloud.com>
2024-08-03 15:21:38 -0400

    Add MapLegend.vue component.

---
+++ src/components/MapLegend.vue
@@ -0,0 +1,82 @@
+<script setup>
+</script>
+
+<template>
+    <div class="map-legend">
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#fff" stroke="#bdbdbd"></rect>
+            </svg>
+            <figcaption>no data</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#e9f1ff"></rect>
+            </svg>
+            <figcaption>&lt; 1.5</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#d0e4ff"></rect>
+            </svg>
+            <figcaption>1.5 - 1.9</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#9ccaff"></rect>
+            </svg>
+            <figcaption>2 - 2.4</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#6bb0f6"></rect>
+            </svg>
+            <figcaption>2.5 - 2.9</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#4e95d9"></rect>
+            </svg>
+            <figcaption>3 - 3.4</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#ffdcc0"></rect>
+            </svg>
+            <figcaption>3.5 - 3.9</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#ffb874"></rect>
+            </svg>
+            <figcaption>4 - 4.4</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#ec9840"></rect>
+            </svg>
+            <figcaption>4.5 - 4.9</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#ffb4ab"></rect>
+            </svg>
+            <figcaption>5 - 5.4</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#ff897d"></rect>
+            </svg>
+            <figcaption>5.5 - 5.9</figcaption>
+        </figure>
+        <figure class="legend">
+            <svg class="colored-sq">
+                <rect width="24px" height="24px" fill="#ff5449"></rect>
+            </svg>
+            <figcaption>6+</figcaption>
+        </figure>
+    </div>
+</template>
+
+<style scoped>
+</style>
