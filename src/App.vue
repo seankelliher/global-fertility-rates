8c78b0365164d7ea780b80724d94de66e75167f6
Sean Kelliher <sbkelliher@icloud.com>
2024-08-03 15:17:18 -0400

    Remove AboutMethods.vue. Reorder other components.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import HeaderPart from "./components/HeaderPart.vue";
 import WorldMap from "./components/WorldMap.vue";
-import AboutMethods from "./components/AboutMethods.vue";
 import SearchNations from "./components/SearchNations.vue";
 import RegionalAvgs from "./components/RegionalAvgs.vue";
 import AllNations from "./components/AllNations.vue";
@@ -13,10 +12,9 @@ import AllNations from "./components/AllNations.vue";
         <WorldMap />
         <main>
             <div class="about">
-                <AboutMethods />
+                <SearchNations />
             </div>
             <div class="statistics">
-                <SearchNations />
                 <AllNations />
                 <RegionalAvgs />
             </div>
