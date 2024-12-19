ff07ca8d986f335e1f33c45d999b9f8d1723c05d
Sean Kelliher <sbkelliher@icloud.com>
2024-12-19 16:03:26 -0500

    Add title and text classes.

--- src/components/MapCaption.vue
+++ src/components/MapCaption.vue
@@ -3,7 +3,13 @@
 
 <template>
     <div class="map-caption">
-        <p>This map uses Total Fertility Rate (TFR), the average number of children that would be born per woman if all women lived to the end of their childbearing years and bore children according to a given fertility rate at each age.</p>
+        <div class="title">
+            <h2>About the data</h2>
+        </div>
+
+        <div class="text">
+            <p class="no-margin-top">This map uses Total Fertility Rate (TFR) from the U.S. Central Intelligence Agency's World Factbook. TFR is the average number of children that would be born per woman if all women lived to the end of their childbearing years and bore children according to a given fertility rate at each age.</p>
+        </div>
     </div>
 </template>
 
