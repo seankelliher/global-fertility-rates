836fd9e6b87c9a22705d127eeb3b35f89421deb1
Sean Kelliher <sbkelliher@icloud.com>
2025-08-02 15:31:39 -0400

    Add new component IntroInfo.vue.

---
+++ src/components/IntroInfo.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div class="intro-box">
+        <div class="intro-box-content">
+            <p class="larger-txt no-margin-top larger-bottom">Fertility rates around the globe vary widely. Women in high-rate nations may have three or four times the number of children as those in low-rate ones. And while demographers favor a stable rate around the replacement rate of 2.1, politicians and pundits weigh in with ideas of their own.</p>
+
+            <p class="larger-txt larger-bottom">A method called <strong>Age-Specific Fertility Rate (ASFR)</strong> generally divides women of childbearing age into five-year bands (for example, 24 - 29) and measures the actual number of children they are producing in a year. ASFR is good at identifying patterns  such as if women are having children earlier or later in life.</p>
+
+            <p class="larger-txt">ASFR forms the basis of the <strong>Total Fertility Rate (TFR)</strong> method. TFR calculates the number of children a women would have if she were to instantaneously progress through her childbearing years with the current ASFR for each age band. TFR is generally the measurement used to compare fertility rates among nations and is what we will use here. Our data comes from the Central Intelligence Agency (CIA).</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
